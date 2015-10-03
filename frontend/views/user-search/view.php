08413b5734f9dd1a3387a3a4bddb062c506dda4d
arfo90 <arfo.amireza@gmail.com>
2015-10-03 17:26:39 +0800

    Cleaning up view from db password and metadate colomns

--- frontend/views/user-search/view.php
+++ frontend/views/user-search/view.php
@@ -28,18 +28,9 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
-            'id',
             'username',
-            'auth_key',
-            'password_hash',
-            'password_reset_token',
             'email:email',
-            'status',
-            'created_at',
-            'updated_at',
-            'mobile_number',
             'mobileNumber',
-            'addressImage',
         ],
     ]) ?>
 
